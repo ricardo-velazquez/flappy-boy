0b6bae44aef5ff64b260836835e0702a23072d2d
Ricardo Velazquez <ricardo.vel7903@gmail.com>
2021-08-22 14:30:51 -0700

    Add structs to player

---
+++ src/entities/player.c
@@ -0,0 +1,7 @@
+#include <gb/gb.h>
+
+struct Player {
+    UINT8 x;
+    UINT8 y;
+
+}
--- src/main.c
+++ src/main.c
@@ -6,8 +6,12 @@
 #include "tilemap.c"
 
 
-void bootstrap();
+void setup();
+void setup_player();
 void update();
+
+void move_game_object();
+
 
 
 UINT8 wait_acc = 0;
@@ -25,15 +29,25 @@ INT8 jump_limit =25;
 UINT8 acc  = 0;
 UINT8 limit  = 1;
 
+struct player {
+    UBYTE sprite_id;
+    UINT8 x;
+    UINT8 y;
+    UINT8 width;
+    UINT8 height;
+}
+
+
+
 void main () { 
-    bootstrap();
+    setup();
     while (1) {
         update();
         wait_vbl_done();
         delay(5);
     }
 }
-void bootstrap() {
+void setup() {
     // Setup Player
 
 
@@ -41,8 +55,8 @@ void bootstrap() {
     set_bkg_tiles(0, 0, 40, 18, tilemap);
 
     set_sprite_data(0, 1, sprites);
-    set_sprite_tile(0, 0);
-    move_sprite(0, 40, 60);
+    setup_player();
+    
     //set_sprite_prop(0, get_sprite_prop(0) & ~S_PRIORITY);
 //set_sprite_prop(0, S_PRIORITY);
     SHOW_BKG;
@@ -67,7 +81,8 @@ void update() {
 
     if (state == 1) {
         current_jump_acc += 3;
-        scroll_sprite(0, 0, -3);
+        //scroll_sprite(0, 0, -3);
+        move_game_object(&player, 0, -3);
         if (current_jump_acc >= jump_limit) {
             state = 3;
             current_jump_acc = 0;
@@ -92,6 +107,21 @@ void update() {
     }
 }
 
+
+
+void setup_player() {
+    player->sprite_id = 0;
+    player->x = 40;
+    player->y = 60;
+    player->width  = 8;
+    player->height = 8;
+    set_sprite_tile(player->sprite_id, player->sprite_id);
+    move_sprite(player->sprite_id, player->x, player->y);
+}
+
+void move_game_object(struct* obj, UINT8 x, UINT8 y) {
+    move_sprite(obj->sprite_id, obj->x + x , obj->y + y);
+}
 
 
 
