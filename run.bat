a9949c0701f7ee4a3c278e3c9b1139a995dc8c6c
Ricardo Velazquez <ricardo.vel7903@gmail.com>
2021-08-22 16:43:55 -0700

    Add gitingore file, Update run.bat to create folder if it does not exist

--- run.bat
+++ run.bat
@@ -1,7 +1,6 @@
 REM Automatically generated from Makefile
-del dist/dist.gb
-del dist/dist.map
-del dist/dist.noi
+if not exist "dist" mkdir dist
+del dist/*
 C:\gbdk\bin\lcc -c -o dist/main.o src/main.c
 C:\gbdk\bin\lcc -c -o dist/tilemap.o src/tilemap.c
 C:\gbdk\bin\lcc -o dist/dist.gb dist/main.o dist/tilemap.o
